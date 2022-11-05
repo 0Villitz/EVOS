5da627eda12ffa910a10641001bc0ece19e44ad9
0Villitz <99107910+0Villitz@users.noreply.github.com>
2022-11-04 18:21:53 -0700

    Minor script alteration

--- Assets/_Scenes/TestScenes/NickTests/Cat_Scripts/CatDoor.cs
+++ Assets/_Scenes/TestScenes/NickTests/Cat_Scripts/CatDoor.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class CatDoor : MonoBehaviour
 {
+    // Int to load level by Build Index:
     public int lvlToLoad;
+
+    // Can also load by name:
+    //public string lvl;
 
     void Start()
     {
@@ -17,10 +21,17 @@ public class CatDoor : MonoBehaviour
         SceneManager.LoadScene(lvlToLoad);
     }
 
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            GetComponent<BoxCollider2D>().enabled = false;
+            collision.GetComponent<GatherInput>().DisableControls();
             LoadLevel();
         }
     }
