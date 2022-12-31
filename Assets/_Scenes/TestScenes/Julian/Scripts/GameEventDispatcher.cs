681079003d6e3d28e5348223b65fa45b4bd2909c
Xaer0 <jdfrag@gmail.com>
2022-12-31 02:39:02 -0800

    Add event dispatcher to allow different scripts and systems to communicate

---
+++ Assets/_Scenes/TestScenes/Julian/Scripts/GameEventDispatcher.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameEventManager", menuName = "EVOS/GameEventManager")]
+public class GameEventDispatcher : ScriptableObject, IEventDispatcher
+{
+    private NotificationDispatcher _dispatcher;
+
+
+    public void AddListener(string eventKey, Action<GeneralEvent> callback)
+    {
+        dispatcher.AddListener(eventKey, callback);
+    }
+    public void RemoveListener(string eventKey, Action<GeneralEvent> callback)
+    {
+        dispatcher.RemoveListener(eventKey, callback);
+    }
+    public bool HasListener(string eventKey)
+    {
+        return dispatcher.HasListener(eventKey);
+    }
+    public void RemoveAllListenersOfEvent(string eventKey)
+    {
+        dispatcher.RemoveAllListenersOfEvent(eventKey);
+    }
+    public void RemoveAllListeners()
+    {
+        dispatcher.RemoveAllListeners();
+    }
+    public bool DispatchEvent(string eventKey, bool bubbles = false, object eventData = null)
+    {
+        return dispatcher.DispatchEvent(eventKey, bubbles, eventData);
+    }
+    public bool DispatchEvent(GeneralEvent e)
+    {
+       return dispatcher.DispatchEvent(e);
+    }
+
+    private NotificationDispatcher dispatcher
+    {
+        get
+        {
+            if(_dispatcher == null)
+            {
+                _dispatcher = new NotificationDispatcher();
+            }
+            return _dispatcher;
+        }
+    }
+}
