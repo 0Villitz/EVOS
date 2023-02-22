8fe964db6267584c9d1564da2fa772ec2eb2e0e7
Xaer0 <jdfrag@gmail.com>
2023-02-21 22:40:51 -0800

    Add terminal lockout functionality

--- Assets/_Scenes/TestScenes/Julian/Scripts/Interaction/PuzzleTerminal.cs
+++ Assets/_Scenes/TestScenes/Julian/Scripts/Interaction/PuzzleTerminal.cs
@@ -16,9 +16,11 @@ public class PuzzleTerminal : MonoBehaviour, IPlayerInteractable
     public int       _RandomLevelCount;
     public List<PuzzleBase> _SpecificLevelList;
 
+    private float _unlockTime;
     
     public float InteractCutoffDistance => _InteractableCutoffDistance;
-    
+
+
     public void Interact()
     {
         if (string.IsNullOrEmpty(_TriggerKey))
@@ -26,22 +28,45 @@ public class PuzzleTerminal : MonoBehaviour, IPlayerInteractable
             Debug.LogError($"Puzzle Terminal:{name} cannot have an empty Trigger key!");
             return;
         }
-        
-        var showPuzzleArgs = new ShowPuzzleArgs
+
+        if (Time.time < _unlockTime)
         {
-            PuzzleType = _PuzzleType,
-            TriggerKey = _TriggerKey,
-        };
-        
-        if (_RandomLevelCount > 0)
-        {
-            showPuzzleArgs.RandomLevelCount = _RandomLevelCount;
+            PlayLockoutVfx();
         }
         else
         {
-            showPuzzleArgs.SpecificLevelList = _SpecificLevelList.ToList();
+            ShowPuzzle();
         }
 
-        _GameEventDispatcher.DispatchEvent(GameEventType.ShowPuzzleWindow, showPuzzleArgs);
+
+        void PlayLockoutVfx()
+        {
+            Debug.Log($"Locked out of Terminal! Unlocks in: {_unlockTime - Time.time} seconds!");
+        }
+
+        void ShowPuzzle()
+        {
+            var showPuzzleArgs = new ShowPuzzleArgs
+            {
+                PuzzleType = _PuzzleType,
+                TriggerKey = _TriggerKey,
+            };
+            
+            if (_RandomLevelCount > 0)
+            {
+                showPuzzleArgs.RandomLevelCount = _RandomLevelCount;
+            }
+            else
+            {
+                showPuzzleArgs.SpecificLevelList = _SpecificLevelList.ToList();
+            }
+
+            _GameEventDispatcher.DispatchEvent(GameEventType.ShowPuzzleWindow, showPuzzleArgs);
+        }
     }
+
+    public void Lockout(float lockoutTimeInSeconds)
+    {
+        _unlockTime = Time.time + lockoutTimeInSeconds;
+    } 
 }
