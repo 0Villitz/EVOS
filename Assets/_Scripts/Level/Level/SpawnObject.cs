a71896f56fe08b3daebcef4f3a1911a6ac125581
luchoscar <losh177@hotmail.com>
2023-04-08 01:48:08 -0700

    Lsaenz/monster anim (#35)
    * adding animations
    
    * adding npc attack
    
    * cleaning up scene and animator controller
    
    * cleaning up controllers
    
    - base controller to handle player/npc common logic
    - action controller initialized based on character controller data
    - cleaning up prefab
    
    * setting states to be specified in prefabs rather than code
    
    * cleaning up prefabs and code
    
    * cleaning up npc controller and adding it to test scene
    
    * cleaning up
    
    * fixing animation event
    
    * removing commented code
    
    * attack state transitions to spawn due to character controller not detecting ground for some reason
    
    * npc brain can be setup via prefab
    
    * adding respawn point as last interact object that implements IPlayerRespawn and fixing health for respawn
    
    * killing player only if current health <= 0
    
    * adding respawn transition and fixing npc losing player ref
    
    * cleaning up
    
    * reverting some changes
    
    * linking fade out on respawn and cleaning up
    
    * fixing animation event keys and connecting for player death/respawn animation events
    
    * adding blackout for players
    
    * removing hack and setting up default state
    
    * adding delay for npc spawn and reder textures for debug
    
    * removing debugs
    
    * npc character controller disabled in prefab and enable 1 frame after spawn

--- Assets/_Scripts/Level/Level/SpawnObject.cs
+++ Assets/_Scripts/Level/Level/SpawnObject.cs
@@ -8,8 +8,7 @@ public class SpawnObject : MonoBehaviour
     [SerializeField] private Transform _npcSection;
     [SerializeField] private NPCController [] _npcPrefabs;
     [SerializeField] private List<NavigationNode> _path;
-    [SerializeField] private float _spawnDelay = 5f;
-    
+
     public void SpawnNPC(Transform player)
     {
         StartCoroutine(SpawnNPCCorutine(player));
@@ -17,11 +16,14 @@ public class SpawnObject : MonoBehaviour
 
     private IEnumerator SpawnNPCCorutine(Transform player)
     {
-        yield return new WaitForSeconds(_spawnDelay);
+        // yield return new WaitForSeconds(_spawnDelay);
         int npcIdx = Random.Range(0, _npcPrefabs.Length - 1);
 
         NPCController npc = GameObject.Instantiate(_npcPrefabs[npcIdx], _npcSection);
         npc.transform.position = transform.position;
+        
+        yield return new WaitForEndOfFrame();
+        
         npc.Initialize(_path, player.GetComponent<IPlayerCharacter>());
     }
 }
